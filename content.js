b1176e47a8e2a69d5dd35888617d9d45b992a651
bfresnel <5629642+bfresnel@users.noreply.github.com>
2023-10-28 23:01:03 +0200

    Updating zip extension file + added button for debug purpose

--- content.js
+++ content.js
@@ -1,5 +1,3 @@
-console.log('👨‍💻 Author: Saurav Hathi \n🌟 GitHub: https://github.com/sauravhathi \n🚀Linkedin: https://www.linkedin.com/in/sauravhathi');
-
 /**
  * Watches for the presence of a YouTube ad blocker popup and removes it.
  * Also adds a "Donate" button to the video owner's profile.
@@ -9,17 +7,14 @@ function watchForElement() {
   const dialogSelector = "tp-yt-paper-dialog";
   const playButtonSelector = ".ytp-play-button.ytp-button";
 
-  const donateBtn = document.createElement("button");
-  donateBtn.innerHTML = "Donate";
-  donateBtn.classList.add("donate-btn");
-  donateBtn.addEventListener("click", () => {
-    window.open("https://github.com/sauravhathi/youtube-ad-blocker-popup-removal#support-the-developer");
-  });
-
   const observer = new MutationObserver(function (mutations) {
     const backdrop = document.querySelector(backdropSelector);
     const dialog = document.querySelector(dialogSelector);
     const playButton = document.querySelector(playButtonSelector);
+    const p = document.querySelector("div#owner");
+
+    const specialButton = document.createElement("button");
+    specialButton.innerHTML = "Yo poto :)"
 
     // If the backdrop and dialog exist, remove them.
     if (backdrop) {
@@ -32,7 +27,9 @@ function watchForElement() {
       if (playButton) {
         playButton.click();
       }
-
+      if (p) {
+        p.appendChild(specialButton);
+      }
       observer.disconnect();
     }
 
