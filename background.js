ebc2c5e7ca5e9c03b8c665ffa0faedb3bb4ca157
bfresnel <5629642+bfresnel@users.noreply.github.com>
2023-10-28 21:59:54 +0200

    Deleted donation.html and ads zip for easier install

--- background.js
+++ background.js
@@ -1,7 +1,3 @@
-chrome.runtime.onInstalled.addListener(function () {
-    chrome.tabs.create({ url: 'donation.html' });
-});
-
 (async () => {
     await chrome.alarms.create("donation", { periodInMinutes: 14400 });
     chrome.alarms.onAlarm.addListener(function (alarm) {
